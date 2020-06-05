42980dab1f474878da15b3507c3c73462f1b4542
Weidi-Sun <33800578+Weidi-Sun@users.noreply.github.com>
2020-06-05 22:21:15 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# Global Robustness Verification Networks
+﻿# Global Robustness Verification Networks
 
-Here we provide the code of SDN in "net_model.py",the back-propagation algorithm in "multpro_back_pro.py", and the verification algorithm in "GeometricDescriptionSample.py".
-
+Here we provide the code of all case studys in Section5. The architecture of SDN is in "net_model.py", "train_SDN.py" shows how to train SDN, the back-propagation algorithm is in "multpro_back_pro.py", and the verification algorithm is in "GeometricDescriptionSample.py".
 
 Take the case study in Section 5.3 as an example, we provide some intermediate results as well.
 Such as the net model "model / net.pkl" and the rule-based back-propagation result "rule_TREE".
 
 
-All the  commands  for the result in Section 5.3 are in "GeometricDescriptionSample.py" where "g_info" is the architecture of SDN, "cons" is the input classification rule, and "configs" is the bound of inputs.
+The example commands  of the result in Section 5.3 are in "GeometricDescriptionSample.py" where "g_info" is the architecture of SDN, "cons" is the input classification rule, and "configs" is the bound of inputs.
 
 
-What's more， "GeometricDescriptionSample.py" returns all the connected components and the classifications which are sorted by their volume and "r" respectively.
+Besides the verification result， "GeometricDescriptionSample.py" returns all the connected components and the classifications which are sorted by their volume and "r" respectively.
 
 
-If you want to test this algorithm, please run "GeometricDescriptionSample.py".
+If you want to test the verification algorithm, please run "GeometricDescriptionSample.py".
---
+++ train_SDN.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+
+import torch.optim as optim
+import warnings
+from net_model import Net
+from generate_data import G
+import pickle
+from tensorboardX import SummaryWriter
+
+
+
+writer = SummaryWriter('log')
+warnings.filterwarnings("ignore")
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+batch_size = 256
+
+
+
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters())
+step = 600000
+
+def train(step):
+    max = 0
+    record = 0
+    train_record  = 0
+    train_time = 0
+    for i in range(step):
+        _images, _labels = g.train_next_batch(batch_size)
+        images = torch.from_numpy(_images).type(torch.float)
+        _,labels = torch.max(torch.from_numpy(_labels), 1)
+        images = images
+        labels = labels
+        optimizer.zero_grad()
+        outputs, succ, train = net(images)
+        goal = criterion(outputs, labels)
+
+        if succ<0.8:
+            loss = goal+ 1e-5*train
+            train_record += 1e-5*train
+            train_time += 1
+        else:
+            loss = goal
+        loss.backward()
+        optimizer.step()
+
+        info = {'loss': goal}
+        for tag, value in info.items():
+            writer.add_scalar(tag, value, i)
+        record += goal
+
+        if i % 1000 == 0:
+            correct = 0
+            total = 0
+            _test_images = g.test_images
+            _test_labels = g.test_labels
+            test_images = torch.from_numpy(_test_images).type(torch.float)
+            _,test_labels =  torch.max(torch.from_numpy(_test_labels), 1)
+            outputs, succ, _ = net(test_images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += test_labels.size(0)
+            correct += (predicted == test_labels).sum().item()
+            accuracy_value = correct/total
+            judge_result,_,_ = net(torch.tensor([[0,0]]).float())
+            if accuracy_value>max and judge_result[0][0]>judge_result[0][1]:
+                max = accuracy_value
+                torch.save(net, 'model/net.pkl')
+                with open('output.txt', 'a') as f:
+                    print('max', file=f)
+            with open('output.txt', 'a') as f:
+                print('step:%d accuracy:%.4f  loss:%.4f train:%.6f succ:%.4f' % (i, accuracy_value, record / 1000, (train_record/train_time) if train_time !=0 else 0, succ),file=f)
+            record = 0
+            train_record = 0
+            train_time = 0
+if __name__ == '__main__':
+    data_set = open("data", "rb")
+    g = pickle.load(data_set)
+    data_set.close()
+    train(step)
